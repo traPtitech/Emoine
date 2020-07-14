f5ef69ee1e68ebd5a2c20ccce9b317659e3b2309
sapphi-red <green@sapphi.red>
2020-07-14 23:10:28 +0900

    管理画面用コンポーネントのひな型

---
+++ client/src/components/Admin/Statistics.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>統計情報</h3>
+    <p>すべての発表の統計が見れる(リアクションもレビューも)</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Statistics',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style module>
+.re {
+  font-weight: bold;
+}
+</style>
