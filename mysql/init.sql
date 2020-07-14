b6a59d706097263e1539d545a1cdf35e58bada1f
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2020-07-15 01:42:07 +0900

    fix

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS state
 (
     id        SMALLINT AUTO_INCREMENT,
-    state     TINYTEXT NOT NULL,
-    info      TEXT,
-    createdAt DATETIME NOT NULL,
+    state     TINYTEXT NOT NULL DEFAULT 'pause',
+    info      TEXT              DEFAULT '準備中...',
+    createdAt DATETIME,
+    updatedAt DATETIME,
     PRIMARY KEY (`id`)
 );
 
@@ -20,24 +21,33 @@ CREATE TABLE IF NOT EXISTS presentation
 
 CREATE TABLE IF NOT EXISTS reaction
 (
-    id        SMALLINT AUTO_INCREMENT,
-    userId    VARCHAR(32)      NOT NULL,
-    team      TINYINT UNSIGNED NOT NULL,
-    stamp     TINYTEXT,
-    comment   TEXT,
-    createdAt DATETIME,
+    id             SMALLINT AUTO_INCREMENT,
+    userId         VARCHAR(32)      NOT NULL,
+    presentationId TINYINT UNSIGNED NOT NULL,
+    stamp          TINYTEXT,
+    createdAt      DATETIME,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS comment
+(
+    id             SMALLINT AUTO_INCREMENT,
+    userId         VARCHAR(32)      NOT NULL,
+    presentationId TINYINT UNSIGNED NOT NULL,
+    text           TEXT,
+    createdAt      DATETIME,
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS review
 (
-    userId       VARCHAR(32),
-    teamId       SMALLINT UNSIGNED,
-    skill        TINYINT UNSIGNED NOT NULL,
-    artistry     TINYINT UNSIGNED NOT NULL,
-    idea         TINYINT UNSIGNED NOT NULL,
-    presentation TINYINT UNSIGNED NOT NULL,
-    createdAt    DATETIME,
-    updatedAt    DATETIME,
-    PRIMARY KEY (userId, teamId)
+    userId         VARCHAR(32),
+    presentationId SMALLINT UNSIGNED,
+    skill          TINYINT UNSIGNED NOT NULL,
+    artistry       TINYINT UNSIGNED NOT NULL,
+    idea           TINYINT UNSIGNED NOT NULL,
+    presentation   TINYINT UNSIGNED NOT NULL,
+    createdAt      DATETIME,
+    updatedAt      DATETIME,
+    PRIMARY KEY (`userId`, `presentationId`)
 );
