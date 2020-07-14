3433beaeec4454a8d9f857e280aa61e9c1b13b7a
sapphi-red <green@sapphi.red>
2020-07-14 23:15:42 +0900

    視聴画面用のコンポーネントのひな型

---
+++ client/src/components/Review.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>Review</h3>
+    <!--
+      - レビュー
+        - する
+        - 編集できるように
+    -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Review',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style module>
+.re {
+  font-weight: bold;
+}
+</style>
