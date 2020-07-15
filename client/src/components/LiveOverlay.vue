ce1ba0166ce117718fcf446d9a0bd0c1528d6fa9
sapphi-red <green@sapphi.red>
2020-07-15 21:26:54 +0900

    overlay toggle

--- client/src/components/LiveOverlay.vue
+++ client/src/components/LiveOverlay.vue
@@ -1,15 +1,20 @@
 <template>
-  <div :class="$style.overlay">
-    <h3>LiveOverlay {{ liveId }}</h3>
-    <!--
-      - リアクション
-      - コメント
-    -->
+  <div :class="$style.container">
+    <div v-if="show" :class="$style.overlay">
+      <h3>LiveOverlay {{ liveId }}</h3>
+      <!--
+        - リアクション
+        - コメント
+      -->
+    </div>
+    <button :class="$style.toggle" @click="toggle">
+      Toggle overlay
+    </button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue'
+import { defineComponent, computed, ref } from 'vue'
 import { useStore } from '/@/store'
 
 export default defineComponent({
@@ -17,12 +22,26 @@ export default defineComponent({
   setup() {
     const store = useStore()
     const liveId = computed(() => store.state.liveId)
-    return { liveId }
+
+    const show = ref(true)
+    const toggle = () => {
+      show.value = !show.value
+    }
+
+    return { liveId, show, toggle }
   }
 })
 </script>
 
 <style lang="scss" module>
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  pointer-events: none;
+}
 .overlay {
   position: absolute;
   top: 0;
@@ -30,5 +49,13 @@ export default defineComponent({
   bottom: 0;
   right: 0;
   background: rgba(255, 0, 0, 0.1);
+  pointer-events: all;
+}
+.toggle {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background: rgba(255, 255, 255, 0.8);
+  pointer-events: all;
 }
 </style>
