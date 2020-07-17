f6a597c0eb0139bbd4b930b2b3d44e39dc018780
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2020-07-18 02:50:06 +0900

    chores

--- Makefile
+++ Makefile
@@ -4,9 +4,18 @@ PROTO_TARGETS = comment reaction state message
 proto:
 	for t in $(PROTO_TARGETS); do \
 		echo "protobuf build ... $$t"; \
-		protoc -I=./docs --go_out=./router/handler ./docs/$$t.proto; \
+		protoc -I=./docs --go_out=./router ./docs/$$t.proto; \
 	done
 
 .PHONY: build
 build:
 	docker build -t emoine .
+
+
+.PHONY: up
+up:
+	@docker-compose up -d --build
+
+.PHONY: down
+down:
+	@docker-compose down -v
--- router/router.go
+++ router/router.go
@@ -2,27 +2,27 @@ package router
 
 import (
 	"github.com/FujishigeTemma/Emoine/repository"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Handlers struct {
-	Repo           repository.Repository
+	Repo repository.Repository
 }
 
-func Setup(r repository.Repository) *echo.Echo {
+func Setup(repo repository.Repository) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.Logger())
-  e.Use(middleware.Recover())
+	e.Use(middleware.Recover())
 	h := &Handlers{
-		Repo: r,
+		Repo: repo,
 	}
 
 	api := e.Group("/api")
 	{
-		apiPresentaions := api.Group("/presentations")
+		apiPresentations := api.Group("/presentations")
 		{
-			apiPresentaions.GET("", h.GetPresentations)
+			apiPresentations.GET("", h.GetPresentations)
 		}
 	}
 	return e
