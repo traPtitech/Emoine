f601b88c1f82cea3ce5b378775b383f8b7cc21a2
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2020-07-18 14:35:30 +0900

    wip

--- router/session.go
+++ router/session.go
@@ -2,8 +2,10 @@ package router
 
 import (
 	"github.com/gofrs/uuid"
-	"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
+	"net/http"
 	"sync"
+	"time"
 )
 
 type Session interface {
@@ -16,8 +18,53 @@ type session struct {
 	key    string
 	userID uuid.UUID
 	sync.RWMutex
+	req      *http.Request
 	conn     *websocket.Conn
 	streamer *Streamer
 	open     bool
-	send     chan *Message
+	send     chan *rawMessage
+}
+func (s *session) writeMessage(msg *rawMessage) error {
+	if s.closed() {
+		return ErrAlreadyClosed
+	}
+
+	select {
+	case s.send <- msg:
+	default:
+		return ErrBufferIsFull
+	}
+	return nil
+}
+
+func (s *session) write(messageType int, data []byte) error {
+	_ = s.conn.SetWriteDeadline(time.Now().Add(writeWait))
+	return s.conn.WriteMessage(messageType, data)
+}
+
+func (s *session) close() {
+	if !s.closed() {
+		s.Lock()
+		s.open = false
+		s.conn.Close()
+		close(s.send)
+		s.Unlock()
+	}
+}
+
+func (s *session) closed() bool {
+	s.RLock()
+	defer s.RUnlock()
+
+	return !s.open
+}
+
+// Key implements Session interface.
+func (s *session) Key() string {
+	return s.key
+}
+
+// UserID implements Session interface.
+func (s *session) UserID() uuid.UUID {
+	return s.userID
 }
--- router/streamer.go
+++ router/streamer.go
@@ -2,8 +2,14 @@ package router
 
 import (
 	"errors"
+	"github.com/gofrs/uuid"
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
 	"github.com/leandro-lugaresi/hub"
+	"math/rand"
+	"net/http"
 	"sync"
+	"unsafe"
 )
 
 var (
@@ -25,7 +31,7 @@ type Streamer struct {
 
 // NewStreamer WebSocketストリーマーを生成し起動します
 func NewStreamer(hub *hub.Hub) *Streamer {
-	h := &Streamer{
+	s := &Streamer{
 		sessions:   make(map[*session]struct{}),
 		register:   make(chan *session),
 		unregister: make(chan *session),
@@ -33,8 +39,8 @@ func NewStreamer(hub *hub.Hub) *Streamer {
 		open:       true,
 	}
 
-	go h.run()
-	return h
+	go s.run()
+	return s
 }
 
 func (s *Streamer) run() {
@@ -54,7 +60,7 @@ func (s *Streamer) run() {
 
 		case <-s.stop:
 			s.mu.Lock()
-			m := &Message{
+			m := &rawMessage{
 				t:    websocket.CloseMessage,
 				data: websocket.FormatCloseMessage(websocket.CloseServiceRestart, "Server is stopping..."),
 			}
@@ -68,4 +74,89 @@ func (s *Streamer) run() {
 			return
 		}
 	}
+}
+
+// ServeHTTP http.Handlerインターフェイスの実装
+func (s *Streamer) ServeHTTP(c echo.Context) {
+	if s.IsClosed() {
+		http.Error(c.Response(), http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+		return
+	}
+
+	conn, err := upgrader.Upgrade(c.Response(), c.Request(), c.Response().Header())
+	if err != nil {
+		return
+	}
+
+	session := &session{
+		key:      randomAlphaNumeric(20),
+		userID:   c.Request().Context().Value(extension.CtxUserIDKey).(uuid.UUID),
+		req:      c.Request(),
+		conn:     conn,
+		open:     true,
+		streamer: s,
+		send:     make(chan *rawMessage, messageBufferSize),
+	}
+
+	s.register <- session
+
+	go session.writeLoop()
+	session.readLoop()
+
+	s.vm.RemoveViewer(session)
+	_ = s.webrtc.ResetState(session.Key(), session.UserID())
+	s.hub.Publish(hub.Message{
+		Name: event.WSDisconnected,
+		Fields: hub.Fields{
+			"user_id": session.UserID(),
+			"req":     r,
+		},
+	})
+	wsConnectionCounter.Dec()
+	s.unregister <- session
+	session.close()
+}
+
+// IsClosed ストリーマーが停止しているかどうか
+func (s *Streamer) IsClosed() bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return !s.open
+}
+
+// Close ストリーマーを停止します
+func (s *Streamer) Close() error {
+	if s.IsClosed() {
+		return ErrAlreadyClosed
+	}
+	s.stop <- struct{}{}
+	return nil
+}
+
+const (
+	rs6Letters       = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	rs6LetterIdxBits = 6
+	rs6LetterIdxMask = 1<<rs6LetterIdxBits - 1
+	rs6LetterIdxMax  = 63 / rs6LetterIdxBits
+)
+
+// randomAlphaNumeric 指定した文字数のランダム英数字文字列を生成します
+// この関数はmath/randが生成する擬似乱数を使用します
+func randomAlphaNumeric(n int) string {
+	b := make([]byte, n)
+	cache, remain := rand.Int63(), rs6LetterIdxMax
+	for i := n - 1; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), rs6LetterIdxMax
+		}
+		idx := int(cache & rs6LetterIdxMask)
+		if idx < len(rs6Letters) {
+			b[i] = rs6Letters[idx]
+			i--
+		}
+		cache >>= rs6LetterIdxBits
+		remain--
+	}
+	return *(*string)(unsafe.Pointer(&b))
 }
