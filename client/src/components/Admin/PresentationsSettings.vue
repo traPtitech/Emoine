a22f929e45142a17b7bbe601f329f785a38e3d52
sapphi-red <green@sapphi.red>
2020-07-19 01:35:44 +0900

    統計表示の準備

--- client/src/components/Admin/PresentationsSettings.vue
+++ client/src/components/Admin/PresentationsSettings.vue
@@ -12,6 +12,7 @@
 <script lang="ts">
 import { defineComponent, ref, onMounted, computed } from 'vue'
 import apis, { Presentation } from '/@/lib/apis'
+import { linkedListMapToArray } from '/@/lib/util'
 import PresentaionComponent from './Presentation.vue'
 
 export default defineComponent({
@@ -21,24 +22,9 @@ export default defineComponent({
   },
   setup() {
     const presentationMap = ref(new Map<number, Presentation>())
-    const presentationList = computed(() => {
-      const list: Presentation[] = []
-      const first = [...presentationMap.value.values()].find(
-        p => p.prev === null
-      )
-
-      let now = first ?? null
-      while (now !== null) {
-        list.push(now)
-        if (now.next) {
-          now = presentationMap.value.get(now.next) ?? null
-        } else {
-          now = null
-        }
-      }
-
-      return list
-    })
+    const presentationList = computed(() =>
+      linkedListMapToArray(presentationMap.value)
+    )
 
     onMounted(async () => {
       const { data } = await apis.getPresentations()
