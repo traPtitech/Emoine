52e8e50f343958b65cd9b12efb43bf80acbb4ce9
sapphi-red <green@sapphi.red>
2020-07-18 19:58:38 +0900

    WebSocketの受け取り

--- client/src/lib/connect.ts
+++ client/src/lib/connect.ts
@@ -1,10 +1,42 @@
 import { getWs } from '/@/lib/ws'
-import { Message, IMessage } from '/@/lib/pb'
+import { Message, IMessage, IReaction, IComment } from '/@/lib/pb'
+
+export const ConnectTarget = document.createDocumentFragment()
+
+const onReaction = (m: Message) => {
+  if (!m.reaction) return
+  ConnectTarget.dispatchEvent(
+    new CustomEvent('reaction', {
+      detail: m.reaction
+    })
+  )
+}
+
+const onComment = (m: Message) => {
+  if (!m.comment) return
+  ConnectTarget.dispatchEvent(
+    new CustomEvent('comment', {
+      detail: m.comment
+    })
+  )
+}
 
 export const setup = (): void => {
   const ws = getWs()
   ws.addEventListener('message', e => {
-    console.log(Message.decode(new Uint8Array(e.data)))
+    const message = Message.decode(new Uint8Array(e.data))
+    switch (message.payload) {
+      case 'reaction': {
+        onReaction(message)
+        break
+      }
+      case 'comment': {
+        onComment(message)
+        break
+      }
+      default:
+        console.log(message)
+    }
   })
 }
 
@@ -12,4 +44,12 @@ export const send = (m: IMessage): void => {
   const ws = getWs()
   const buff = Message.encode(m).finish()
   ws.send(buff.buffer.slice(buff.byteOffset, buff.byteOffset + buff.length))
+}
+
+const sendReaction = (reaction: Required<IReaction>): void => {
+  send({ reaction })
+}
+
+const sendComment = (comment: Required<IComment>): void => {
+  send({ comment })
 }
