714cf8017632ea8a88641cfea08582c8aa045b47
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-07-18 22:59:36 +0900

    wip auth

--- go.mod
+++ go.mod
@@ -6,12 +6,15 @@ require (
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/gofrs/uuid v3.3.0+incompatible
 	github.com/golang/protobuf v1.4.2
+	github.com/gorilla/sessions v1.1.3
 	github.com/gorilla/websocket v1.4.2
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/json-iterator/go v1.1.10
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/labstack/echo-contrib v0.9.0
 	github.com/labstack/echo/v4 v4.1.16
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/traPtitech/traQ v1.0.0-rc.2
+	go.uber.org/zap v1.15.0
 	google.golang.org/protobuf v1.25.0
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- router/router.go
+++ router/router.go
@@ -1,26 +1,44 @@
 package router
 
 import (
+	"net/http"
+	"os"
+	"strings"
+
 	"github.com/FujishigeTemma/Emoine/repository"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 type Handlers struct {
 	Repo repository.Repository
+	SessionOption     sessions.Options
+	ClientID          string
 }
 
 func Setup(repo repository.Repository) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
+
 	h := &Handlers{
 		Repo: repo,
+		SessionOption: sessions.Options{
+			Path:     "/",
+			MaxAge:   86400 * 30,
+			HttpOnly: true,
+			SameSite: http.SameSiteLaxMode,
+		},
+		ClientID:   os.Getenv("CLIENT_ID"),
 	}
+	e.Use(h.WatchCallbackMiddleware())
 
-	s := NewStreamer()
+	// s := NewStreamer()
 
-	api := e.Group("/api")
+	api := e.Group("/api", h.TraQUserMiddleware)
 	{
 		apiPresentations := api.Group("/presentations")
 		{
@@ -32,10 +50,19 @@ func Setup(repo repository.Repository) *echo.Echo {
 				apiPresentationsID.PATCH("", h.PatchPresentation)
 			}
 		}
-		api.Group("/ws").GET("", func(c echo.Context) error {
-			s.ServeHTTP(c)
-			return nil
-		})
+		// api.Group("/ws").GET("", func(c echo.Context) error {
+		// 	s.ServeHTTP(c)
+		// 	return nil
+		// })
 	}
+	e.POST("/api/authParams", h.HandlePostAuthParams)
+
+	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Skipper: func(c echo.Context) bool {
+			return strings.HasPrefix(c.Request().URL.Path, "/api")
+		},
+		Root:  "web/dist",
+		HTML5: true,
+	}))
 	return e
 }
