86edefd5794939b8807e6a3ce449e68e7c00101d
sapphi-red <green@sapphi.red>
2021-01-17 17:11:51 +0900

    bump package version

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,26 @@
 import { UserConfig } from 'vite'
+import VuePlugin from '@vitejs/plugin-vue'
+// @ts-expect-error prevent installing @types/node
 import path from 'path'
+
+declare const __dirname: string
 
 const config: UserConfig = {
   alias: {
-    '/@/': path.resolve(__dirname, 'src')
+    '/@': path.resolve(__dirname, 'src')
   },
-  proxy: {
-    '/api': {
-      target: 'http://localhost:3050',
-      changeOrigin: true
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3050',
+        changeOrigin: true
+      }
     }
   },
   optimizeDeps: {
     include: ['protobufjs/minimal']
-  }
+  },
+  plugins: [VuePlugin()]
 }
 
 export default config
