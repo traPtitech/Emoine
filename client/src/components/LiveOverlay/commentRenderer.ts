c1987ecb07398a5c19e91ebd1f1ee52a6b9ff2b9
o137 <85795175+o137@users.noreply.github.com>
2021-06-30 21:25:08 +0900

    コメントのフォントサイズに下限を設定 fix #86

--- client/src/components/LiveOverlay/commentRenderer.ts
+++ client/src/components/LiveOverlay/commentRenderer.ts
@@ -1,14 +1,42 @@
 import { Ref } from 'vue'
 
-const CommentLines = 10
-let commentLineNow = 0
+/*
+- フォントサイズ:  画面に CommentLines 行表示できるサイズ。ただし、最低でも MinimumFontSize px
+- コメント表示位置: 上から詰める
+  - 現状、コメントはその文字長に関わらず画面内にいる間行を専有する(ニコニコでは文字列幅だけ専有している)
+*/
 
-const incrementCommentLine = () => {
-  commentLineNow++
-  if (commentLineNow === CommentLines) {
-    commentLineNow = 0
+const CommentLines = 10
+const MinimumFontSize = 20
+const LineHeight = 1.5
+
+class FlowController {
+  private countPerLine = Array(CommentLines).fill(0) // 行ごとのコメント数
+  private lineCount = CommentLines // 画面に表示される行数
+  private lineHeight = MinimumFontSize * LineHeight
+
+  updateVideoHeight(height: number) {
+    this.lineCount = Math.min(
+      Math.floor(height / (MinimumFontSize * LineHeight)),
+      CommentLines
+    )
+    this.lineHeight = height / this.lineCount
+  }
+  next(): { top: number; fontSize: number; release: () => void } {
+    const countPerLine = this.countPerLine.slice(0, this.lineCount)
+    const minCount = Math.min(...countPerLine)
+    const nextIndex = countPerLine.findIndex(o => o === minCount)
+
+    this.countPerLine[nextIndex]++
+    return {
+      top: nextIndex * this.lineHeight,
+      fontSize: this.lineHeight / LineHeight,
+      release: () => this.countPerLine[nextIndex]--
+    }
   }
 }
+
+const flowController = new FlowController()
 
 export const addComment = (
   baseEle: Ref<HTMLDivElement | undefined>,
@@ -17,22 +45,22 @@ export const addComment = (
 ): void => {
   if (!baseEle.value) return
 
-  const lineHeight = baseHeight.value / CommentLines
+  flowController.updateVideoHeight(baseHeight.value)
+  const { top, fontSize, release } = flowController.next()
 
   const $comment = document.createElement('div')
   $comment.className = 'animation-comment'
   $comment.textContent = text
-  $comment.style.top = `${commentLineNow * lineHeight}px`
-  $comment.style.fontSize = `${lineHeight}px`
+  $comment.style.top = `${top}px`
+  $comment.style.fontSize = `${fontSize}px`
 
   $comment.addEventListener(
     'animationend',
     () => {
       $comment.remove()
+      release()
     },
     { once: true }
   )
   baseEle.value.append($comment)
-
-  incrementCommentLine()
 }
