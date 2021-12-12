9cade2a39cd1b36dfc9185a1f069913af36918e1
sapphi-red <green@sapphi.red>
2021-12-12 17:22:22 +0900

    tokenの生成の実装

--- utils/random.go
+++ utils/random.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	crand "crypto/rand"
+	"math/big"
 	"math/rand"
 	"sync"
 	"time"
@@ -38,4 +40,18 @@ func RandAlphabetAndNumberString(n int) string {
 	}
 	randSrcPool.Put(randSrc)
 	return string(b)
+}
+
+// SecureRandomString 小文字英字と数字からなるランダムな暗号論的にセキュアな文字列を生成する
+func SecureRandomString(n int) (string, error) {
+	const letters = "0123456789abcdefghijklmnopqrstuvwxyz"
+	ret := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, err := crand.Int(crand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "", err
+		}
+		ret[i] = letters[num.Int64()]
+	}
+	return string(ret), nil
 }
